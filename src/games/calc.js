595ef8c5ed96be2e670eec829176cc4d8945b0ca
Lasterius <zrk.vlg@gmail.com>
2023-01-23 15:27:08 +0300

    fix linter errors

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,9 +6,8 @@ const condition = 'What is the result of the expression?';
 const refreshEveryRound = () => {
   const randomNumber = getRandomInteger(1, 20);
   const randomNumber2 = getRandomInteger(1, 20);
-  const arrayOfRandomOperators = ['+', '-', '*'];
-  const randomOperator = 
-    arrayOfRandomOperators[getRandomInteger(0, arrayOfRandomOperators.length - 1)];
+  const operators = ['+', '-', '*'];
+  const randomOperator = operators[getRandomInteger(0, operators.length - 1)];
   const question = `${randomNumber} ${randomOperator} ${randomNumber2}`;
   const resultOfOperation = calculate(randomOperator, randomNumber, randomNumber2);
   const rightAnswer = String(resultOfOperation);
