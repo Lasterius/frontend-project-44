739c6a50d936de7bb1ddc3b31a7529290c5f3b4e
Lasterius <zrk.vlg@gmail.com>
2023-01-23 15:36:11 +0300

    fix error

--- src/games/even.js
+++ src/games/even.js
@@ -5,7 +5,7 @@ const condition = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const refreshEveryRound = () => {
   const question = getRandomInteger(1, 20);
-  const rightAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  const rightAnswer = isEven(question) ? 'yes' : 'no';
   return [question, rightAnswer];
 };
 
